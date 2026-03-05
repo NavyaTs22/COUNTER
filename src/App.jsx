9a94c8688075a22858deebae1d6e5b6c5504fb96
NavyaTs22 <navyatsbsccse25@rvu.edu.in>
2026-03-05 15:09:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [number, set_number] = useState(0)
+
+  const increment=()=>{
+    set_number(number+1)
+  }
+
+
+  const decrement=()=>{
+    if(number>0) set_number(number-1)
+  }
+
+  const reset=()=>{
+    set_number(0)
+  }
+
+  return (
+    
+    <div>
+      <h1>COUNTER APP</h1>
+      <br></br>
+      {number}
+      <br></br>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>RESET</button>
+    </div> 
+    
+    
+  )
+}
+
+export default App
